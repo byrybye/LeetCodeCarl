06539cd1ee2c20787ecc9b6ccfd467cc85e1956f
byrybye <byrybye@hotmail.com>
2022-11-19 21:45:02 +0800

    代码增加注释

--- CSharp/LeetCode/Code27.cs
+++ CSharp/LeetCode/Code27.cs
@@ -8,6 +8,13 @@ namespace LeetCode
 {
     public class Code27
     {
+        /// <summary>
+        /// 双指针 感觉需要 N的平方 或 对数 的复杂，
+        /// 要求 N 的复杂度解决，需要考虑双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
         public int RemoveElement(int[] nums, int val)
         {
             int slow = 0;
