ae459f2d101394487b41b30503038e738c615b63
byrybye <byrybye@hotmail.com>
2022-11-19 20:17:41 +0800

    代码更新

---
+++ CSharp/LeetCode/LeetCode/Code27.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Code27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int slow = 0;
+            int fast = 0;
+            for(fast = 0; fast < nums.Length; fast++)
+            {
+                if(nums[fast] != val)
+                {
+                    nums[slow] = nums[fast];
+                    slow += 1;
+                }
+            }
+            return slow;
+
+        }
+    }
+}
---
+++ CSharp/LeetCode/LeetCode/Code704.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Code704
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length < 1)
+            {
+                return -1;
+            }
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                if (target < nums[middle])
+                {
+                    right = middle - 1;
+                }
+                else if (target > nums[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+            return -1;
+        }
+
+        public void Test()
+        {
+            int[] mums = { 5 };
+            var index = Search(mums, -5);
+            Console.WriteLine(index);
+
+        }
+    }
+}
